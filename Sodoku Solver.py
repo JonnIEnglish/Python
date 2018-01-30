27383e57278f00f1c1b1e77b20b23f23c6af6f21
Jonno <jonnovdriet@gmail.com>
2018-01-30 20:48:11 +0200

    The first version of the Sodoku solver. Main structure in place like blocks etc.

---
+++ Sodoku Solver.py
@@ -0,0 +1,141 @@
+import numpy as np
+
+"""
+A  1  2  3  4  5  6  7  8  9
+B
+C
+D
+E
+F
+G
+H
+I
+"""
+def assign():
+
+
+
+    a1 = 0
+    a2 = 0
+    a3 = 0
+    a4 = 0
+    a5 = 0
+    a6 = 0
+    a7 = 0
+    a8 = 0
+    a9 = 0
+
+    b1 = 0
+    b2 = 0
+    b3 = 0
+    b4 = 0
+    b5 = 0
+    b6 = 0
+    b7 = 0
+    b8 = 0
+    b9 = 0
+
+    c1 = 0
+    c2 = 0
+    c3 = 0
+    c4 = 0
+    c5 = 0
+    c6 = 0
+    c7 = 0
+    c8 = 0
+    c9 = 0
+
+    d1 = 0
+    d2 = 0
+    d3 = 0
+    d4 = 0
+    d5 = 0
+    d6 = 0
+    d7 = 0
+    d8 = 0
+    d9 = 0
+
+    e1 = 0
+    e2 = 0
+    e3 = 0
+    e4 = 0
+    e5 = 0
+    e6 = 0
+    e7 = 0
+    e8 = 0
+    e9 = 0
+
+    f1 = 0
+    f2 = 0
+    f3 = 0
+    f4 = 0
+    f5 = 0
+    f6 = 0
+    f7 = 0
+    f8 = 0
+    f9 = 0
+
+    g1 = 0
+    g2 = 0
+    g3 = 0
+    g4 = 0
+    g5 = 0
+    g6 = 0
+    g7 = 0
+    g8 = 0
+    g9 = 0
+
+    h1 = 0
+    h2 = 0
+    h3 = 0
+    h4 = 0
+    h5 = 0
+    h6 = 0
+    h7 = 0
+    h8 = 0
+    h9 = 0
+
+    i1 = 0
+    i2 = 0
+    i3 = 0
+    i4 = 0
+    i5 = 0
+    i6 = 0
+    i7 = 0
+    i8 = 0
+    i9 = 0
+
+
+    rowA = np.matrix([a1, a2, a3, a4, a5, a6, a7, a8, a9])
+    rowB = np.matrix([b1, b2, b3, b4, b5, b6, b7, b8, b9])
+    rowC = np.matrix([c1, c2, c3, c4, c5, c6, c7, c8, c9])
+    rowD = np.matrix([d1, d2, d3, d4, d5, d6, d7, d8, d9])
+    rowE = np.matrix([e1, e2, e3, e4, e5, e6, e7, e8, e9])
+    rowF = np.matrix([f1, f2, f3, f4, f5, f6, f7, f8, f9])
+    rowG = np.matrix([g1, g2, g3, g4, g5, g6, g7, g8, g9])
+    rowH = np.matrix([h1, h2, h3, h4, h5, h6, h7, h8, h9])
+    rowI = np.matrix([i1, i2, i3, i4, i5, i6, i7, i8, i9])
+
+    column1 = np.matrix([[a1], [a2], [a3], [a4], [a5], [a6], [a7], [a8], [a9]])
+    column2 = np.matrix([[b1], [b2], [b3], [b4], [b5], [b6], [b7], [b8], [b9]])
+    column3 = np.matrix([[c1], [c2], [c3], [c4], [c5], [c6], [c7], [c8], [c9]])
+    column4 = np.matrix([[d1], [d2], [d3], [d4], [d5], [d6], [d7], [d8], [d9]])
+    column5 = np.matrix([[e1], [e2], [e3], [e4], [e5], [e6], [e7], [e8], [e9]])
+    column6 = np.matrix([[f1], [f2], [f3], [f4], [f5], [f6], [f7], [f8], [f9]])
+    column7 = np.matrix([[g1], [g2], [g3], [g4], [g5], [g6], [g7], [g8], [g9]])
+    column8 = np.matrix([[h1], [h2], [h3], [h4], [h5], [h6], [h7], [h8], [h9]])
+    column9 = np.matrix([[i1], [i2], [i3], [i4], [i5], [i6], [i7], [i8], [i9]])
+
+    B2 = np.matrix([[a1, a2, a3], [b1, b2, b3], [c1, c2, c3]])
+    B5 = np.matrix([[a4, a5, a6], [b4, b5, b6], [c4, c5, c6]])
+    B8 = np.matrix([[a7, a8, a9], [b7, b8, b9], [c7, c8, c9]])
+    E2 = np.matrix([[d1, d2, d3], [e1, e2, e3], [f1, f2, f3]])
+    E5 = np.matrix([[d4, d5, d6], [e4, e5, e6], [f4, f5, f6]])
+    E8 = np.matrix([[d7, d8, d9], [e7, e8, e9], [f7, f8, f9]])
+    H2 = np.matrix([[g1, g2, g3], [h1, h2, h3], [i1, i2, i3]])
+    H5 = np.matrix([[g4, g5, g6], [h4, h5, h6], [i4, i5, i6]])
+    H8 = np.matrix([[g7, g8, g9], [h7, h8, h9], [i7, i8, i9]])
+
+
+for A1 in range (1,10):
+    if
